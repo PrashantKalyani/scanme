8aa2419293eab967457e23554116ff8d5ec0bc6a
AdarshMShet <adarshmshet@gmail.com>
2024-05-21 17:35:01 +0530

    "adding files"

--- src/main/java/com/desapex/spacemanager/domain/Compressor.java
+++ src/main/java/com/desapex/spacemanager/domain/Compressor.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 public class Compressor {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "equip_name")
