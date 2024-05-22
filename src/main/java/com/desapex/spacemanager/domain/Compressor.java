f0fdf13e0f89f94d3f3abbdb9d1c3b2058620f7d
AdarshMShet <adarshmshet@gmail.com>
2024-05-22 10:51:26 +0530

    "adding files"

--- src/main/java/com/desapex/spacemanager/domain/Compressor.java
+++ src/main/java/com/desapex/spacemanager/domain/Compressor.java
@@ -47,9 +47,6 @@ public class Compressor {
     @Column(name = "limit_p1_p2")
     private String limitP1P2;
 
-    @Column(name = "c_out_pressure")
-    private Double cOutPressure;
-
     @Column(name = "dp_air_filter")
     private Double dpAirFilter;
 
@@ -59,9 +56,6 @@ public class Compressor {
     @Column(name = "oil_injection_pressure")
     private Double oilInjectionPressure;
 
-    @Column(name = "c_out_temp")
-    private Double cOutTemp;
-
     @Column(name = "element_out_temp")
     private Double elementOutTemp;
 
@@ -70,6 +64,12 @@ public class Compressor {
 
     @Column(name = "oil_seperator")
     private String oilSeperator;
+
+    @Column(name = "c_out_pressure")
+    private Double cOutPressure;
+
+    @Column(name = "c_out_temp")
+    private Double cOutTemp;
 
     @Column(name = "remarks")
     private String remarks;
